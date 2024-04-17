5e3c995843e9a63de91398f5e9607608d13f4e27
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-04-17 16:32:29 +0500

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,42 +1,70 @@
 import React from "react";
 import Head from '../views/global/global/Head';
+import css from "../styles/index.css"
+import line from '../images/Line1.png'
+import Popup from 'reactjs-popup';
+import main_woman from '../images/main_woman.png'
+import line_2 from '../images/Line2.png'
 
-function Png()
+function Main_woman()
 {
-  return <img className= 'png' src='/ellipse 1.png'></img>
+  return <img className= 'png' src={main_woman}></img>
 }
 function Line1()
 {
-  return <img className= 'line1' src='/Линия 1.png'></img>
+  return <img className= 'line1' src={line}></img>
 }
 function Line2()
 {
-  return <img className= 'line2' src='/Vector 3.png'></img>
-}
-function Letter()
-{
-  return <img className= 'letter' src='/письмо_главное.png'></img>
+  return <img className= 'line2' src={line_2}></img>
 }
 function Feedback()
 {
-  return <button className='feedback'>Посмотреть обратную связь</button>
+  return <Popup trigger= {<button className='feedback'>Посмотреть обратную связь</button>}
+  modal nested>
+    {
+close => ( 
+  <div className='modalWindow'>
+    <Close/>
+    <div className='text'>
+    Если у вас уже есть ключ от обратной связи, <br/>
+    введите его ниже, чтобы проверить статус вашего обращения<br/>
+    <input className="key" placeholder = {"Ввести ключ..."}></input>
+    <button className="check">Проверить обратную связь</button>
+    </div>
+  </div>
+)
+    }
+    </Popup>
 }
-function Application()
-{
-  return <button className ='application'>Подать заявку</button>
+
+class Close extends React.Component {
+  onclick(){
+window.location.assign('/')
+  }
+  render() {
+    return <div className ='close' onClick={(e) => this.onclick(e)}></div>
+  }
+}
+
+class Application extends React.Component {
+  onclick(){
+window.location.assign('/application')
+  }
+  render() {
+    return <button className ='application' onClick={(e) => this.onclick(e)}>Оставить заявку</button>
+  }
 }
 
 const Main = () => {
     return (
         <React.Fragment>
             <Head></Head>
-            <Png></Png>
+            <Main_woman></Main_woman>
             <Line1></Line1>
             <Line2></Line2>
             <Application></Application>
-            <Letter></Letter>
-            <Feedback></Feedback>
-            <h1>Развиваемся вместе: ваше мнение для нас важно!</h1>
+            <Feedback></Feedback> 
         </React.Fragment>
     )
 }
