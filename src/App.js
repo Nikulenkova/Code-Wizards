16f28903c84c4753c9d84bbe01ca6095e9a71f3f
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-04-17 16:22:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import '../src/styles/index.css'
 import Main from './pages/Main';
-import '../src/styles/Login.css'
 import Login from './pages/Login';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Application from './pages/Application';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Appeals from './pages/Appeals'
 
 function App() {
+  <Main/>
   return (
-  <React.Fragment>
-     <Main/>
-   </React.Fragment>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element = {<Main/>}/>
+          <Route path="/login" element = {<Login/>}/>
+          <Route path="/application" element = {<Application/>}/>
+          <Route path="/appeals" element = {<Appeals/>}/>
+        </Routes>
+      </div>
+    </Router>
   )
  }
  export default App;
-  //   (
-  //    <BrowserRouter><Routes>
-  //     <Route path='/' element = {<Main/>}>
-  //       <Route path='one' element = {<Login/>}/>
-  //     </Route>
-  //    </Routes>
-  //    </BrowserRouter>
-  //  )
-
-// function App() {
-//       return(
-//           <Router>
-//               <div className='App'>
-//                   <Route path ="/" exact component={Main}/>
-//                  <Route path="/second" exact component={Login}/>
-//              </div>
-//          </Router>
-//       )
-//   }
-//   export default App;
-
-//  class App extends React.Component {
-//    render() {
-//      return (
-//        <div className="App">
-//          <h1>Hello, React!</h1>
-//        </div>
-//      )
-//    }
-//  }
-// // ReactDOM.render(<App />, document.getElementById('root'))
