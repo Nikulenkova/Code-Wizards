f760dc7d99ddde73fa2265d5713bd6b5196392e7
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-04-18 18:21:47 +0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Head2 from "../views/global/global/Head2";
 import InputComponent from "../comps/Input";
+import css from "../styles/Login.css"
+import Table from '../Tablet.png'
+import human from '../Human.png'
+import rectangle from '../прямоугольник.png'
+import circle from '../Круг.png'
 
 function Tablet()
 {
-  return <img className= 'tablet' src='/Tablet.png'></img>
+  return <img className= 'tablet' src={Table}></img>
 }
 function Human()
 {
-  return <img className= 'human' src='/Human.png'></img>
+  return <img className= 'human' src={human}></img>
 }
 function Rectangle()
 {
-  return <img className= 'rectangle' src='/прямоугольник.png'></img>
+  return <img className= 'rectangle' src={rectangle}></img>
 }
 function Circle()
 {
-  return <img className= 'circle' src='/круг.png'></img>
+  return <img className= 'circle' src={circle}></img>
 }
+
+class Vxod extends React.Component {
+  onclick(){
+window.location.assign('/appeals')
+  }
+  render() {
+    return <button className="Vxod" onClick={(e) => this.onclick(e)}>Войти</button>
+  }
+}
+
 const Login = () => {
+
     return (
         <React.Fragment>
             <Head2></Head2>
             <Tablet/>
             <Human/>
-            <h1 className="n">Добро пожаловать!</h1>
-            <form>
-            <InputComponent placeholder = {"Логин"}/>
-            <InputComponent placeholder = {"Пароль"}/> 
+            <h2 className="n">Добро пожаловать!</h2>
+            <form className="form1">
+            <InputComponent name = 'login' placeholder = {"Логин"}/>
+            <InputComponent name = 'password' placeholder = {"Пароль"}/> 
             </form>
-            <button className="Vxod">Войти</button>
+           <Vxod/> 
             <Rectangle/>
             <Circle/>
         </React.Fragment>
         )
     }
+
     export default Login
