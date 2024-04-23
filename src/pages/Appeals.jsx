7bfa1563d5e9a4d0a2544b1d9de314c5f0d94004
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-04-23 20:59:18 +0500

    Update Appeals.jsx

--- src/pages/Appeals.jsx
+++ src/pages/Appeals.jsx
@@ -5,13 +5,42 @@ import appeals1 from '../images/appeals1.png'
 import appeals2 from '../images/appeals2.png'
 import appeals3 from '../images/appeals3.png'
 
+class Completed extends React.Component {
+    onclick() {
+       window.location.assign('/completed_requests');
+    }
+    render() {
+        return (<button className="image-button3" onClick={(e) => this.onclick(e)}><img src={appeals3}></img><br/>Завершенные обращения</button>)
+    }
+ }
+ class New extends React.Component {
+    onclick() {
+       window.location.assign('/new_requests');
+    }
+    render() {
+        return (<button className="image-button1" onClick={(e) => this.onclick(e)}><img src={appeals1}></img><br/>Новые обращения</button>)
+    }
+ }
+
+ class In_work extends React.Component {
+    onclick() {
+       window.location.assign('/requests_in_work');
+    }
+    render() {
+        return (<button className="image-button2" onClick={(e) => this.onclick(e)}><img src={appeals2}></img><br/>Обращения в работе</button>)
+    }
+ }
 const Appeals = () => {
     return (
         <React.Fragment>
-            <Head2></Head2>
-            <button className="image-button1"><img src={appeals1}></img><br/>Новые обращения</button>
-            <button className="image-button2"><img src={appeals2}></img><br/>Обращения в работе</button>
-            <button className="image-button3"><img src={appeals3}></img><br/>Завершенные обращения</button>
+            <div className="container">
+            <div className="data-section">
+            <Head2></Head2> 
+            <New/>
+            <In_work/>
+            <Completed/>
+            </div>
+            </div>
         </React.Fragment>
         )
     }
