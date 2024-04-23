5454864d68c5cd9f4abe8e5c83d0d8067d29f922
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-04-23 21:01:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Application from './pages/Application';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Appeals from './pages/Appeals'
+import New_requests from './pages/New_requests';
+import Requests_in_work from './pages/Requests_in_work';
+import Completed_requests from './pages/Completed_requests';
+import Appeals from './pages/Appeals';
 
 function App() {
-  <Main/>
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element = {<Main/>}/>
-          <Route path="/login" element = {<Login/>}/>
-          <Route path="/application" element = {<Application/>}/>
-          <Route path="/appeals" element = {<Appeals/>}/>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/application" element={<Application />} />
+        <Route path="/appeals" element={<Appeals />} />
+        <Route path="/new_requests" element={<New_requests />} /> 
+        <Route path="/requests_in_work" element={<Requests_in_work />} />
+        <Route path="/completed_requests" element={<Completed_requests />} />
+      </Routes>
     </Router>
-  )
- }
- export default App;
+  );
+}
+
+export default App;
