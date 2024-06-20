1a87ac01ebeba52faaf89deadc05e72e0dad3105
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-20 15:45:51 +0500

    Update InputFile.jsx

--- src/comps/InputFile.jsx
+++ src/comps/InputFile.jsx
@@ -2,8 +2,14 @@ import "./styles.css"
 
 export const InputFile = ({accept, multiple, files, setFiles}) => {
     const handlChange = (event) =>{
-    const files = event.target.files;
-    setFiles([...files]);
+        const files = event.target.files;
+        setFiles([...files]);
+    };
+
+    const handleDelete = (index) => {
+        const updatedFiles = [...files];
+        updatedFiles.splice(index, 1);
+        setFiles(updatedFiles);
     };
 
     return(
@@ -13,10 +19,12 @@ export const InputFile = ({accept, multiple, files, setFiles}) => {
                 <span className="input-file-placeholder">Загрузить фотографию</span>
             </label>
             <div className="c-1">
-            {files && files.length > 0 && files.map((file,i) =>
-            <p className="input-file-info" key = {i}>
-            {file.name}
-            </p>)}
+                {files && files.length > 0 && files.map((file, i) =>
+                    <div key={i} className="input-file-info">
+                        {file.name}
+                        <span className="delete-icon" onClick={() => handleDelete(i)}>✕</span>
+                    </div>
+                )}
             </div>
         </div>
     )
