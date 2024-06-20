28f31d53d506ef5f999de2ce1e6e9c104a3ff15f
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-20 12:12:14 +0500

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,51 +1,47 @@
 import React from "react";
 import Head from '../views/global/global/Head';
 import css from "../styles/index.css"
-// import ellips from '../images/Ellipse 1.png'
-import line from '../images/Line1.png'
-import vector from '../images/Vector 3.png'
-import letter from '../images/письмо_главное.png'
 import Popup from 'reactjs-popup';
 import main_woman from '../images/main_woman.png'
-import line_2 from '../images/Line2.png'
+import lamp from '../images/Лампа.png'
+import human_main from '../images/Human_main.png'
+import line_main from '../images/Line_main1.png'
 
-function Main_woman()
+function Lamp()
 {
-  return <img className= 'png' src={main_woman}></img>
+  return <img className= 'lamp_png' src={lamp}></img>
 }
-function Line1()
+
+function Human_main()
 {
-  return <img className= 'line1' src={line}></img>
+  return <img className= 'human_png' src={human_main}></img>
 }
-function Line2()
+
+function Line_main()
 {
-  return <img className= 'line2' src={line_2}></img>
+  return <img className= 'line_png' src={line_main}></img>
 }
-function Letter()
-{
-  return <img className= 'letter' src={letter}></img>
-}
+
 function Feedback()
 {
-  return <Popup trigger= {<button className='feedback'>Посмотреть обратную связь</button>}
-  modal nested>
-    {
-close => ( 
-  <div className='modalWindow'>
-    <div className='text'>
-    <Close/>
-    Если у вас уже есть ключ от обратной связи, <br/>
-    введите его ниже, чтобы проверить статус вашего обращения<br/>
-    <div className="d1">
-    <input className="key" placeholder = {"Ввести ключ..."}></input>
-    <button className="check">Проверить обратную связь</button>
-    </div>
-    </div>
-  </div>
-)
-    }
+  return (
+    <Popup trigger={<button className='feedback'>Посмотреть обратную связь</button>} modal nested>
+      {close => (
+        <div className='modalWindow'>
+          <div className='text'>
+            <Close />
+            Если у вас уже есть ключ от обратной связи, <br />
+            введите его ниже, чтобы проверить статус вашего обращения<br />
+            <div className="d1">
+              <input className="key" placeholder={"Ввести ключ..."} ></input>
+              <button className="check">Проверить обратную связь</button>
+            </div>
+          </div>
+        </div>
+      )}
     </Popup>
-}
+    );
+    }
 
 class Close extends React.Component {
   onclick(){
@@ -61,7 +57,7 @@ class Application extends React.Component {
 window.location.assign('/application')
   }
   render() {
-    return <button className ='application' onClick={(e) => this.onclick(e)}>Оставить заявку</button>
+    return <button className ='application' onClick={(e) => this.onclick(e)}>Зарегистрировать заявку</button>
   }
 }
 
@@ -69,13 +65,14 @@ const Main = () => {
     return (
         <React.Fragment>
           <div className="conteiner">
-            <Head></Head>
-            <Main_woman></Main_woman>
-            <Line1></Line1>
-            <Line2></Line2>
-            <Application></Application>
-            {/* <Letter></Letter> */}
-            <Feedback></Feedback> 
+            <Head/>
+            <p className="text_main">Говорите <span className="color">свободно,</span> <br></br>оставайтесь анонимными</p>
+            <p className="text_main1">Оставьте свою заявку и получите обратную связь </p>
+            <Lamp/>
+            <Human_main/>
+            <Line_main/>
+            <Application/>
+            <Feedback/> 
             </div>
         </React.Fragment>
     )
