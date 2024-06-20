e4822a1aadfec1b915bd44cd54f4faf40a0aa611
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-20 15:10:32 +0500

    Update Application.jsx

--- src/pages/Application.jsx
+++ src/pages/Application.jsx
@@ -3,20 +3,14 @@ import Head2 from "../views/global/global/Head2";
 import css from "../styles/Application.css"
 import { InputFile } from "../comps/InputFile";
 import { useState } from "react";
-import woman from '../images/woman(application).png'
-import spot from '../images/Пятно(Application).png'
 import copy from '../images/копирование.png'
 import Popup from 'reactjs-popup';
+import appl from '../images/обращение.png'
 
- function Woman() {
- return(
-     <img className ="woman" src={woman}></img>
- )
- }
 
- function Spot() {
+ function Appl() {
     return(
-        <img className ="spot" src={spot}></img>
+        <img className ="apll" src={appl}></img>
     )
     }
 
@@ -25,8 +19,12 @@ import Popup from 'reactjs-popup';
       window.location.assign('/')
         }
         render() {
-          return <button className="main" onClick={(e) => this.onclick(e)}>Перейти на главную</button>
-        }
+            return (
+              <button className="main" onClick={(e) => this.onclick(e)}>
+                <span className="arrow-left"></span>
+              </button>
+            );
+          }
       }
 
 const Application = () => {
@@ -34,19 +32,16 @@ const Application = () => {
     const [inputValue, setInputValue] = useState("");
     return (
         <React.Fragment>
-            <div className="container">
-            <div className="data-section">
-            <Head2></Head2>
-            <div className="d2">
-            <Woman/> 
-            <Spot/>
-            <form className="form2">
-                Введите ваше имя и фамилию по желанию<br/>
+            <Main/>
+            <div className="container_fon">
+           <form className="form_application">
+                <div className="form_right">
+                <p className="text_form">Введите ваше имя и фамилию по желанию</p>
                     <input className = "input2" type="text" name="name"/>
-                Оставьте жалобу или предложение<br/>
-                    <textarea className = "input3" type="text" name="application"/>
-               Загрузите фото(максимум 3 фото)<br/>
-                <InputFile accept =".png,.jpg,.jpeg" multiple = {true} files={photo} setFiles={setPhoto}/>
+                <p className="text_form">Оставьте жалобу или предложение</p>
+                    <textarea className = "text_input" type="text" name="application"/>
+                <p className="text_form">Загрузите фото(максимум 3 фото)</p>
+                <InputFile accept =".png,.jpg,.jpeg" multiple = {true} files={photo} setFiles={setPhoto}/> 
                 <Popup trigger=
                 {<button type="button" className="send">Отправить заявку</button>}
                 modal nested>
@@ -66,9 +61,12 @@ const Application = () => {
                     )
                 }
                 </Popup>
-            </form>
-            </div>
-            </div>
+                </div>
+                <div className="form_left">
+                <label class="icon">SecretVoice</label>
+                <Appl/>
+                </div>
+            </form> 
             </div>
         </React.Fragment>
         )
