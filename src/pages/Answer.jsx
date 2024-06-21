7cacac5a9fc2be8f45b356f0faffe44cc58b8c9c
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-21 19:14:21 +0500

    Update Answer.jsx

--- src/pages/Answer.jsx
+++ src/pages/Answer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import css from "../styles/Reject_request.css"
+import  '../styles/Answer.css'
+import Arrow from '../images/Стрелка_назад.png'
+import Menu from '../comps/Menu'
 import { useState } from "react";
+import Select from 'react-select'
 
 
 //Путь будет меняться???
@@ -10,53 +13,38 @@ const BackButton = () => {
     };
 
     return (
-        <button className="back-button_1" onClick={onClick}>Назад</button>
+        <button className="back-arrow" onClick={onClick}><img src={Arrow}></img></button>
     );
 };
 
-export const File = ({accept, multiple, files, setFiles}) => {
-    const handlChange = (event) =>{
-    const files = event.target.files;
-    setFiles([...files]);
-    };
-
-    return(
-        <div className="input-file-container">
-            <label className="inputFile">
-                <input type="file" className="input-file-element" accept = {accept} multiple={multiple} onChange={handlChange}/>
-                <span className="input-file-placeholder">Загрузить фотографию</span>
-            </label>
-            <div className="c-1">
-            {files && files.length > 0 && files.map((file,i) =>
-            <p className="input-file-info" key = {i}>
-            {file.name}
-            </p>)}
-            </div>
-        </div>
-    )
-}
 
 const Reject_request = () => {
-    const [photo, setPhoto] = useState(null);
-    const [inputValue, setInputValue] = useState("");
+    const options = [
+        { value: 'Кадровые вопросы', label: 'Кадровые вопросы' },
+        { value: 'Техническая поддержка', label: 'Техническая поддержка' },
+        { value: 'Организация работы', label: 'Организация работы' },
+        { value: 'Административные вопросы', label: 'Административные вопросы' },
+        { value: 'Финансовые вопросы', label: 'Финансовые вопросы' },
+      ];
+      const [selectedOption, setSelectedOption] = useState(null);
     return (
         <React.Fragment>
+            <Menu/>
             <BackButton/>
-            <div className="container_1">
-            <div className="data-section">
-            <form className="form_1">
-            <p>Для удобства определите тему обращения</p>
-                    <input className = "input_2" type="text" name="name"/>
-                    <p>Оставьте ответ на обращение</p>
-                    <textarea className = "input_3" type="text" name="application"/>
-                <p>Загрузите фото(максимум 3 фото)</p>
-                <File accept =".png,.jpg,.jpeg" multiple = {true} files={photo} setFiles={setPhoto}/> 
-            </form>
-            </div>
-            <div className="button-container_answer">
-            <button className="send_answer">Отправить</button>
-            </div>
-            </div>
+            <div className="conteiner-answer">
+                <div className="left-answer">
+                <span className="important-text">Важно:</span><br></br>
+                <label className="text-info">При нажатии на стрелочку<br></br>в левом верхнем углу черновик обращения<br></br>автоматически сохраняется в категории<br></br>“Обращения в работе”</label>
+                </div>
+                <div className="right-answer">
+                <p className="text-topic">Для удобства определите тему обращения</p>
+                <input className = "input-topic" type="text" name="name" placeholder="Введите название темы..."/>
+                <Select className="menu-category" placeholder="Определите категорию обращения" options={options} defaultValue={selectedOption} onChange={setSelectedOption} />
+        <p className="answer-appeal">Оставьте ответ на обращение</p>
+        <textarea className = "input-answer" type="text" name="name" placeholder="Введите ответ..."/>
+        <button className="send-answer">Отправить</button>
+        </div>
+        </div>
         </React.Fragment>
         )
     }
