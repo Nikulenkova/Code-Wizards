38a7ee44c32a0b1caffe26a7b05aa0c512cef8f5
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-22 15:39:59 +0500

    Update Check_completed_request.jsx

--- src/pages/Check_completed_request.jsx
+++ src/pages/Check_completed_request.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import css from '../styles/Check_completed_request.css'
-import Popup from 'reactjs-popup';
-import Head2 from '../views/global/global/Head2'
+import '../styles/Check_completed_request.css'
+import Menu from "../comps/Menu";
+import Arrow from '/Users/nikul/ReactProject/first-project/src/images/Стрелка_назад.png' //Путь измени
 
 const Check_completed_request = () => {
     const feedback = {
@@ -12,34 +12,43 @@ const Check_completed_request = () => {
         topic: "Сломан принтер"
     };
 
+    const Back_Arrow = () => {
+        const onClick = () => {
+            window.location.assign('/completed_requests');
+        };
+    
+        return (
+            <button className="back-arrow" onClick={onClick}><img src={Arrow}></img></button>
+        );
+    };
     return (
         <div>
-            <Head2/>
-            <div className="main-container">
-            <h1 className="feedback-title">{feedback.topic}</h1>
-                <div className="left-section">
-                    <h3 className="section-title">Детали обращения</h3>
-                    <form className="form3">
-                    <h3>Пользователь</h3>
-                    <p>{feedback.name}</p>
-                        <h3>Дата регистрации</h3>
-                        <p>{feedback.date}</p>
-                        <h3 htmlFor="userMessage">Обращение</h3>
-                        <p><textarea id="userMessage" className="input3" type="text" readOnly value={feedback.message}/></p>
-                        <h3><img src={feedback.photo} alt="фото 1" /> </h3>
-                    </form>
-                </div>
-                <div className="right-section">
-                    <h3 className="section-title">Решение по обращению</h3>
-                    <form className="form4">
-                        <h3>Дата решения</h3>
-                        <p>{feedback.date}</p>
-                        <h3 htmlFor="userMessage">Решение</h3>
-                        <p><textarea id="userMessage" className="input4" type="text" readOnly value={feedback.message} /> </p>
-                        <h3><img src={feedback.photo} alt="фото 1" /> </h3>
-                    </form>
-                </div>
+            <Menu/>
+            <Back_Arrow/>
+            <div className="main-container-check-request">
+            <h1 className="topic-title-request">{feedback.topic}</h1>
+            <div className="left-check-request">
+            <div className="title-left-column">Детали обращения</div>
+            <div className="body-left">
+            <h3>Пользователь</h3>
+            <p>{feedback.name}</p>
+            <h3>Дата регистрации</h3>
+            <p>{feedback.date}</p>
+            <h3 htmlFor="userMessage">Обращение</h3>
+            <p><textarea id="userMessage" className="text-appeal-request" type="text" readOnly value={feedback.message}/></p>
+            <h3><img src={feedback.photo} alt="фото 1" /> </h3>
             </div>
+            </div>
+            <div className="right-check-request">
+            <div className="title-right-column">Решение по обращению</div>
+            <div className="body-left">
+            <h3>Дата решения</h3>
+            <p>{feedback.date}</p>
+            <h3 htmlFor="userMessage">Решение</h3>
+            <p><textarea id="userMessage" className="text-appeal-request" type="text" readOnly value={feedback.message} /> </p>
+            </div>
+            </div>
+        </div>
         </div>
     );
 };
