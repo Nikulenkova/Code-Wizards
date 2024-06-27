990e921778b541fdca740d25ad102653f0fc5bc8
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-27 13:57:49 +0500

    Update InputFile.jsx

--- src/comps/InputFile.jsx
+++ src/comps/InputFile.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdFileDownload } from "react-icons/md";
 import '../comps/styles.css';
 
-export const InputFile = ({ accept, multiple, files, setFiles }) => {
+export const InputFile = ({ accept, multiple }) => {
+    const [files, setFiles] = useState([]);
+
     const handleChange = (event) => {
-        const selectedFiles = event.target.files;
-        setFiles([...selectedFiles]);
+        const selectedFiles = Array.from(event.target.files);
+        if (files.length + selectedFiles.length > 3) {
+            alert("Вы можете загрузить не более 3 фотографий");
+            return;
+        }
+        setFiles(prevFiles => (prevFiles ? [...prevFiles, ...selectedFiles] : [...selectedFiles]));
     };
+
     const handleDelete = (index) => {
         const updatedFiles = [...files];
         updatedFiles.splice(index, 1);
         setFiles(updatedFiles);
     };
+
     return (
         <div className="input-file-container">
             <input type="file" className="input-file-element" accept={accept} multiple={multiple} onChange={handleChange} />
