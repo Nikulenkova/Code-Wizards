879e9a571562e271c77819bbb6a3f1ceebfc0fbf
Nikulenkova <113360259+Nikulenkova@users.noreply.github.com>
2024-06-27 11:54:35 +0500

    Update InputFile.jsx

--- src/comps/InputFile.jsx
+++ src/comps/InputFile.jsx
@@ -1,31 +1,26 @@
-import "./styles.css"
+import React from "react";
+import { MdFileDownload } from "react-icons/md";
+import '../comps/styles.css';
 
-export const InputFile = ({accept, multiple, files, setFiles}) => {
-    const handlChange = (event) =>{
-        const files = event.target.files;
-        setFiles([...files]);
+export const InputFile = ({ accept, multiple, files, setFiles }) => {
+    const handleChange = (event) => {
+        const selectedFiles = event.target.files;
+        setFiles([...selectedFiles]);
     };
-
     const handleDelete = (index) => {
         const updatedFiles = [...files];
         updatedFiles.splice(index, 1);
         setFiles(updatedFiles);
     };
-
-    return(
+    return (
         <div className="input-file-container">
-            <label className="input-file-label">
-                <input type="file" className="input-file-element" accept = {accept} multiple={multiple} onChange={handlChange}/>
-                <span className="input-file-placeholder">Загрузить фотографию</span>
-            </label>
-            <div className="c-1">
-                {files && files.length > 0 && files.map((file, i) =>
-                    <div key={i} className="input-file-info">
-                        {file.name}
-                        <span className="delete-icon" onClick={() => handleDelete(i)}>✕</span>
-                    </div>
-                )}
-            </div>
+            <input type="file" className="input-file-element" accept={accept} multiple={multiple} onChange={handleChange} />
+            {files && files.length > 0 && files.map((file, i) => (
+                <p className="input-file-info" key={i}>
+                    {file.name}
+                    <span className="delete-icon" onClick={() => handleDelete(i)}>✕</span>
+                </p>
+            ))}
         </div>
-    )
-}
+    );
+};
